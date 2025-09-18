988f7a006323964ea87a439fddef1968c352a966
Tsiory Vahya <132977993+TsioryVahya@users.noreply.github.com>
2025-09-18 11:22:18 +0300

    jwt

--- back/server.js
+++ back/server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const routes = require('./src/routes/routes');
 const { testConnection } = require('./src/config/database');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
